a52790775e96b8340ce545cc527b82de3109e76b
Gerciano Glauco <91155325+GercianoGlauco@users.noreply.github.com>
2024-05-14 02:15:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// ---------- VALIDAÇÃO USERNAME ---------- //
+let usernameInput = document.getElementById("username");
+let usernameLabel = document.querySelector('label[for="username"]');
+let usernameHelper = document.getElementById("username-helper");
+
+ 
+
+//---------- Mostrar popup de campo obrigatório---------- //
+// Campo obrigatório para o User name
+
+
+
+// Campo obrigatório para o Email
+
+
+
+
+//----------Ocultar popup de campo obrigatório----------//
+
+
+//----------Validar valor do input----------//
